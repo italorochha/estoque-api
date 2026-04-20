66561ec310c4be4de589311472d57427eba0869e
Italo Rocha <italorochha00@gmail.com>
2026-04-20 12:20:56 -0300

    debug

--- src/main/java/com/italo/estoque_api/config/SecurityConfig.java
+++ src/main/java/com/italo/estoque_api/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/login", "/api/auth/registrar").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
